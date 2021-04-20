b2453bd72571119a43dbd8bed39048dc34dbf230
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2021-04-20 12:04:22 +0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bbc8c06dd50ce73a418dde2af7ff8229'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eddfe3bb-ee88-43b5-85ff-8bbef5d10508'
+};
